1b189c6c92771a1b0a2111e9f0d3b504a222d648
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-05-07 17:20:21 +0000

    Updated Email Form

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,7 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'adamjn524@gmail.com';
+  $receiving_email_address = 'superiorpaintingllc@icloud.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -37,5 +31,8 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
+  //$contact->cc = array();
+  $contact->bcc = array('adamjn524@gmail.com' );
+  
   echo $contact->send();
 ?>
